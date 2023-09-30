79e3046503c684b08b6c93e8d50bee19ba16af2a
SabrineBaam <baameur.sabrine@gmail.com>
2023-09-30 13:35:26 +0100

    add more certificates

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,7 +7,13 @@ class App extends Component{
    
     render(){
         return(
-                   <Carousel>
+              <Carousel>
+                <Item><img src={require('./images/FullstackEngineeringWithReact&NodeJs.jpg').default} alt=' ' /></Item>
+                <Item><img src={require('./images/FullstackFundamentals.jpg').default} alt=' ' /></Item>
+                <Item><img src={require('./images/BackendEngineeringNodeJsExpress.jpg').default} alt=' ' /></Item>
+                <Item><img src={require('./images/Frontend.jpg').default} alt=' ' /></Item>
+                <Item><img src={require('./images/AdvancedBehaviouralSkills.jpg').default} alt=' ' /></Item>
+                <Item><img src={require('./images/BasicBehaviouralSkills.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/FrontEndLibrariesCer.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/APIs and Microservices.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/DataVisualizationCer.jpg').default} alt=' ' /></Item>
@@ -16,7 +22,6 @@ class App extends Component{
                 <Item><img src={require('./images/ResponsiveWebDesign.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/Git.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/React.jpg').default} alt=' ' /></Item>
-               
             </Carousel> 
             
 
