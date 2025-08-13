ccc5373fdb66bcfb30ac705972b3f11ccab12ac2
SabrineBaam <baameur.sabrine@gmail.com>
2025-08-13 16:10:48 +0100

    modifications

--- src/Carousel.js
+++ src/Carousel.js
@@ -7,10 +7,9 @@ class App extends Component{
     render(){
         return(
             <Carousel>
-                <Item><img src={require('./images/FullstackEngineeringWithReact&NodeJs.jpg').default} alt=' ' /></Item>
+                <Item><img src={require('./images/AWSSAACER.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/AWSCloudPractitioner.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/AWSSolutionArchitectAssociate.jpg').default} alt=' ' /></Item>
-                <Item><img src={require('./images/FullstackEngineeringWithReact&NodeJs.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/FullstackFundamentals.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/BackendEngineeringNodeJsExpress.jpg').default} alt=' ' /></Item>
                 <Item><img src={require('./images/Frontend.jpg').default} alt=' ' /></Item>
